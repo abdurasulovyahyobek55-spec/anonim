b787717fb3f426c831f4a2f07c3040e2a6926943
Yahyobek <abdurasulovyahyobek55@gmail.com>
2026-06-27 07:41:19 +0500

    Update BOT_TOKEN and ADMIN_ID in config.py
    Replaced hardcoded BOT_TOKEN and ADMIN_ID values.

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+BOT_TOKEN = os.getenv("7995346816:AAFM3tc0K418HYQ6KoQzENM2Zt-b3SmkHQU")
+ADMIN_ID = int(os.getenv("8791058696", "0"))
 DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "anonim.db")
